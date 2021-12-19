ac9d245dc2c61723870edcb122660346b1a4595e
tmak1 <tarik.khan.git@gmail.com>
2021-12-20 01:54:40 +1100

    first branch commit

--- cleint/src/App.js
+++ cleint/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import { HelloWorld } from './component/HelloWorld';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
 }
 
 export default App;
